0f6b71266399cc5b6c6531071acbfd1637aa182f
werleycordeiro <31543812+werleycordeiro@users.noreply.github.com>
2019-07-30 16:33:52 -0300

    Add files via upload

---
+++ Nelson_Siegel_factor_loadings.py
@@ -0,0 +1,7 @@
+import numpy as np
+def Nelson_Siegel_factor_loadings(lam,matu):
+ c1 = np.ones(matu.shape[0])
+ c2 = (1-np.exp(-lam*matu))/(lam*matu)
+ c3 = c2-np.exp(-lam*matu)
+ lambmat = np.vstack((c1,c2,c3)).T
+ return(lambmat)
